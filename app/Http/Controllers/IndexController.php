30d56e9498ab3a1edfd0edb723698f4f96bdd858
renzhifan <zhifan6797@163.com>
2018-12-20 15:14:22 +0800

    up

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -26,14 +26,14 @@ class IndexController extends Controller
             if(!$fromAddress || !$toAddress){
                 return ['code'=>400,'message'=>'请填写地址'];
             }
-            if(!$fromAddress[0]==="P" || !$toAddress[0]==="P"){
+            if($fromAddress[0] !== "P" || $toAddress[0] !== "P"){
                 return ['code'=>400,'message'=>'您填写的地址不合法，请重新添加'];
             }
-
             if(!$captcha->check($captchaCode)){
 
                 return ['code'=>400,'message'=>'验证码错误,修改之后请重新提交'];
             }
+
 
             //入库
 
